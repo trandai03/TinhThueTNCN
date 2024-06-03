be24c8f16d820b57676a560f45899823668db53f
trandai03 <21011589@st.phenikaa-uni.edu.vn>
2024-06-03 07:30:12 +0700

    create model tax

---
+++ model/tax.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace model;
+require_once("database.php");
+class tax
+{
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database();
+    }
+
+    public function getAllthue() {
+        return $this->db->getAll('thue');
+    }
+
+    public function getTaxById($id) {
+        return $this->db->getById('thue', $id);
+    }
+
+    public function addTax($thue) {
+        return $this->db->insert('thue', $thue);
+    }
+
+    public function updateTax($thue) {
+        return $this->db->update('thue', $thue);
+    }
+
+    public function deleteTax($id) {
+        return $this->db->delete('thue', $id);
+    }
+
+    public function searchthue($query) {
+        $sql = "SELECT * FROM thue WHERE name LIKE '%$query%' OR description LIKE '%$query%'";
+        return $this->db->exeQuery($sql);
+    }
+}
